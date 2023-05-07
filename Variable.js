89966568742a288c6b63e62bcd3187aa4c92e41b
trungdung70199 <trungdung70199@gmail.com>
2023-05-07 13:32:35 +0900

    Variable in JS

---
+++ Variable.js
@@ -0,0 +1,16 @@
+// Bien cuc bo let
+let myVariable = 10;
+
+// Bien toan cuc var
+var Name = 'Dug';
+
+console.log(Name);
+
+console.log(myVariable);
+
+myVariable
+
+var u;
+console.log(u);
+
+// Hang so (const): Co gia tri bat bien
---
+++ demo.js
@@ -0,0 +1,2 @@
+console.log("Hello World with JS");
+console.log(12234);
