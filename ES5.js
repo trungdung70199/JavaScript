e5369dda3c3f0ae355bc947fae0e6e2f60a10e7b
trungdung70199 <trungdung70199@gmail.com>
2023-08-06 23:40:39 +0900

    Up file

---
+++ ES5.js
@@ -0,0 +1,10 @@
+// Property Access on Strings
+// The chartAt() method returns the character at a 
+// specified index (position) in a string
+// str.chartAt() = str[0]
+
+var str = "Hello World";
+console.log(str.charAt(0));
+
+// Strings Over Multiple lines
+console.log("Hello \ Dug!")
